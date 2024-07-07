00a279618fcfb510b886832d77e52ccb36f092a3
Daniel Watoro <watorondirangu@gmail.com>
2024-07-07 23:03:39 +0300

    Adds new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+const inputField = document.getElementById('new-task-description');
+const taskList = document.getElementById('task-list');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = inputField.value;
+  const listItem = document.createElement('li');
+  listItem.textContent = taskDescription;
+  taskList.appendChild(listItem);
+  inputField.value = '';
+});
 });
